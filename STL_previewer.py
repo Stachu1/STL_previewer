Rotation bugs fixed

STL_previewer.py
@@ -1,5 +1,5 @@
 import time, os, numpy as np, math, random, cv2, sys
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 from struct import unpack
 
 
@@ -14,14 +14,33 @@ class Body:
         self.body = self.load_body()
         self.rotated_body = self.body
         
-        self.center = ((max([vertex[0] for facet in self.body for vertex in facet]) + min([vertex[0] for facet in self.body for vertex in facet])) / 2,
-                       (max([vertex[1] for facet in self.body for vertex in facet]) + min([vertex[1] for facet in self.body for vertex in facet])) / 2,
-                       (max([vertex[2] for facet in self.body for vertex in facet]) + min([vertex[2] for facet in self.body for vertex in facet])) / 2)
+        self.center = self.find_center()
+        self.size = self.find_size()
         
-        self.size = ((max([vertex[0] for facet in self.body for vertex in facet]) - min([vertex[0] for facet in self.body for vertex in facet])),
-                       (max([vertex[1] for facet in self.body for vertex in facet]) - min([vertex[1] for facet in self.body for vertex in facet])),
-                       (max([vertex[2] for facet in self.body for vertex in facet]) - min([vertex[2] for facet in self.body for vertex in facet])))
     
+    def find_center(self):
+        max_x = max([vertex[0] for facet in self.body for vertex in facet])
+        min_x = min([vertex[0] for facet in self.body for vertex in facet])
+        
+        max_y = max([vertex[1] for facet in self.body for vertex in facet])
+        min_y = min([vertex[1] for facet in self.body for vertex in facet])
+        
+        max_z = max([vertex[2] for facet in self.body for vertex in facet])
+        min_z = min([vertex[2] for facet in self.body for vertex in facet])
+        
+        center_x = (max_x + min_x) / 2
+        center_y = (max_y + min_y) / 2
+        center_z = (max_z + min_z) / 2
+        center = (center_x, center_y, center_z)        
+        return center
+        
+    
+    def find_size(self):
+        size = (abs(max([vertex[0] for facet in self.body for vertex in facet]) - min([vertex[0] for facet in self.body for vertex in facet])),
+                abs(max([vertex[1] for facet in self.body for vertex in facet]) - min([vertex[1] for facet in self.body for vertex in facet])),
+                abs(max([vertex[2] for facet in self.body for vertex in facet]) - min([vertex[2] for facet in self.body for vertex in facet])))
+        return size
+        
     
     def load_body(self):
         body = []
@@ -52,8 +71,7 @@ class Body:
                 
                 for facet in facets:
                     body.append([[facet[3], facet[4], facet[5]], [facet[6], facet[7], facet[8]], [facet[9], facet[10], facet[11]]])
-        return body
-                
+        return body     
     
     
     def rotate(self, angle):
@@ -71,19 +89,19 @@ class Body:
                 
                 a = np.deg2rad(angle[1])
                 x = rx - self.center[0]
-                z = point[2] - self.center[1]
+                z = point[2] - self.center[2]
                 rx = x * np.cos(a) - z * np.sin(a)
                 rz = x * np.sin(a) + z * np.cos(a)
                 rx = rx + self.center[0]
-                rz = rz + self.center[1]
+                rz = rz + self.center[2]
                 
                 a = np.deg2rad(angle[0])
-                y = ry - self.center[0]
-                z = rz - self.center[1]
+                y = ry - self.center[1]
+                z = rz - self.center[2]
                 ry = y * np.cos(a) - z * np.sin(a)
                 rz = y * np.sin(a) + z * np.cos(a)
-                ry = ry + self.center[0]
-                rz = rz + self.center[1]
+                ry = ry + self.center[1]
+                rz = rz + self.center[2]
 
                 rotated_body[-1].append([rx, ry, rz])
         return rotated_body
@@ -98,11 +116,20 @@ class Cam:
 
 class Screen:
     def __init__(self, body_center, body_size):
-        self.resolution = [1280, 720]
+        self.resolution = [1600, 900]
+        self.display_size = [1200, 675]
         self.screen_to_body_ratio = 0.8
+        self.line_size = 1
+        self.y_distance_multiplier = 5
+        
         self.size = [body_size[0]/self.screen_to_body_ratio, body_size[0]/self.screen_to_body_ratio * 9/16]
-        self.display_size = [800, 450]
-        self.pos = [body_center[0] - self.size[0]/2, body_center[1]*5, body_center[2] - self.size[1]/2]
+        self.pos = [body_center[0] - self.size[0]/2, body_center[1]*self.y_distance_multiplier, body_center[2] - self.size[1]/2]
+    
+    
+    def add_stats(self, img, msg):
+        draw = ImageDraw.Draw(img)
+        draw.text((10, 10), msg, (255,255,255))
+        return img
         
     
     def project_point(self, point, cam):
@@ -126,9 +153,9 @@ class Screen:
                 if pixel[0] < self.resolution[0] and pixel[1] < self.resolution[1]:
                     corners.append(pixel)
             if len(corners) == 3:
-                draw.line([corners[0], corners[1]], fill=(255,255,255), width=2)
-                draw.line([corners[1], corners[2]], fill=(255,255,255), width=2)
-                draw.line([corners[2], corners[0]], fill=(255,255,255), width=2)
+                draw.line([corners[0], corners[1]], fill=(255,255,255), width=self.line_size)
+                draw.line([corners[1], corners[2]], fill=(255,255,255), width=self.line_size)
+                draw.line([corners[2], corners[0]], fill=(255,255,255), width=self.line_size)
         
         # for facet in body.body:     #* Create list with all vertexes
         #     for vertex in facet:
@@ -156,17 +183,19 @@ screen = Screen(body.center, body.size)
 
 print(body.center, body.size, cam.pos)
 
-# body.rotated_body = body.rotate((0,0,0))
+# body.rotated_body = body.rotate((0,45,0))
 # img = screen.generate_img(body, cam)
 # img.show()
 
-# input("")
 
 r = 0
 while True:
-    for r in range(360):
-        body.rotated_body = body.rotate((0, 0, r))
+    for r in range(720):
+        body.rotated_body = body.rotate((r/2, r/2, r/2))
+
         img = screen.generate_img(body, cam)
+        img  = screen.add_stats(img, f"Rotation: {r/2}")
+        
         open_cv_image = np.array(img) 
         
         cv2.imshow("ELO", open_cv_image)